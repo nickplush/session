44164b264ae7e1aae8d69099bcee770023b5f424
nickplush <zfox109@gmail.com>
2024-11-16 23:11:46 +0300

    new version
    sadasd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,111 @@
+import { useEffect, useState } from 'react';
+import {randomIntFromInterval} from './tools'
 import './App.css';
-import CardWrapper from "./components/CardWrapper/CardWrapper";
+import Answers from './components/Answers';
 
 function App() {
-  return <CardWrapper/>
+  const [variants, setVariants] = useState([])
+  const [selectedAnswer, setSelectedAnswer] = useState()
+  const [myAnswer, setMyAnswer] = useState({})
+  const [checked, setChecked] = useState(false)
+  
+  const [author, setAuthor] = useState([])
+  const [names, setNames] = useState([])
+  const countryes = ['Англия','Голландия','Испания','Италия','Фландрия','Франция']
+
+  const importInages = (countryName, images) => {
+    const authors = new Set(author)
+    const curNames = new Set(names)
+    const imageList = images.keys().map(image => {
+      const slicedImageName = image.split('..') 
+      const author = slicedImageName[0]
+      const name = slicedImageName[1]
+      const src = images(image)
+      const country = countryName
+      authors.add(author)
+      curNames.add(name)
+      return {id:image,author, name, src, country}
+    });
+    setAuthor(Array.from(authors))
+    setNames(Array.from(curNames))
+    return imageList
+  } 
+
+
+  const startNewVariant = () => {
+    const arr = [...variants]
+    const indexElement = randomIntFromInterval(0, arr.length-1)
+    const selected = arr.splice(indexElement, 1)
+    if (!selected[0]) {
+      
+    }
+    setSelectedAnswer(selected[0])
+    setMyAnswer({...selected[0], name: '', country: '', author: ''})
+    setVariants(arr)
+    setChecked(false)
+  }
+
+  const checkToNext = () => {
+    return myAnswer.name && myAnswer.country && myAnswer.author
+  }
+
+  const onMyAnswerChange = (field, value) => {
+    const newAnswer = {...myAnswer}
+    newAnswer[field] = value
+    setMyAnswer(newAnswer)
+  } 
+
+  const check = () => {
+    setChecked(true)
+  }
+
+  useEffect(()=>{
+    const a = require.context('./assets/Голландия', true);
+    const b = require.context('./assets/Испания', true);
+    const c = require.context('./assets/Италия', true);
+    const d = require.context('./assets/Англия', true);
+    const e = require.context('./assets/Франция', true);
+    const f = require.context('./assets/Фландрия', true);
+      const a1 = importInages('Голландия', a)
+      const b1 = importInages('Испания', b)
+      const c1 = importInages('Италия', c)
+      const d1 = importInages('Англия', d)
+      const e1 = importInages('Франция', e)
+      const f1 = importInages('Фландрия', f)
+      const imageList = [...a1,...b1,...c1,...d1,...e1,...f1]
+      setVariants(imageList)
+  },[]) 
+
+
+  return ( 
+  <div className='window-wrapper'>
+    {selectedAnswer 
+    ? <div className='test-conteiner'>
+      <div className='img-conteiner'> 
+        <img src={selectedAnswer.src} onClick={startNewVariant}></img>
+      </div>
+      <div>
+        <Answers 
+        onChangeAnswer={onMyAnswerChange} 
+        currentQwestion={selectedAnswer} 
+        allVariants={{name: names, country: countryes, author}}
+        answer={myAnswer}
+        checked={checked}
+        />
+        {checkToNext() && 
+        <>
+        {checked 
+          ? <div className='button-start' onClick={startNewVariant}>Далее</div>
+          : <div className='button-start' onClick={check}>Проверить</div>
+        }
+        </>
+}
+      </div>
+      </div>
+    : <div className='button-start' onClick={startNewVariant}>Start</div>}
+    
+  </div>
+  )
 }
 
 export default App;
---
+++ src/components/Answers.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import {randomIntFromInterval} from '../tools'
+import './Answer.css';
+
+const Answers = ({onChangeAnswer, currentQwestion, allVariants, answer, checked}) => {
+    const [countryes, setCountryes] = useState([])
+    const [authors, setAuthors] = useState([])
+    const [names, setNames] = useState([])
+
+    const changeVariant = (variantName) => {
+        const indexOfRightvariant = randomIntFromInterval(0, 3)
+        console.log(currentQwestion)
+        console.log(variantName)
+        const selectedTypeAllVariants = [...allVariants[variantName]].filter(item => item !== currentQwestion[variantName])
+        const variants = []
+        for(let i = 0; i < 4; i++) {
+            if (i === indexOfRightvariant) {
+                variants.push(currentQwestion[variantName])
+            } else {
+                const variant = selectedTypeAllVariants.splice(randomIntFromInterval(0, selectedTypeAllVariants.length-1), 1)
+                variants.push(variant[0])
+            }
+        }
+        console.log(variants)
+        return variants
+    }
+
+    const changeStyle = (field ,value) => {
+        let className = 'vaiant'
+        const isSelected = value === answer[field]
+        const isRight = value === currentQwestion[field]
+        if (checked) {
+            if (isSelected) {
+                className += ' error'
+            }
+            if (isRight) {
+                className += ' good'
+            }
+        } else {
+            if (isSelected) {
+                className += ' selected'
+            }
+        }
+        return className
+    }
+    
+    useEffect(()=>{
+        setCountryes(changeVariant('country'))
+        setAuthors(changeVariant('author'))
+        setNames(changeVariant('name'))
+    }, [currentQwestion])
+
+
+  return ( 
+  <div>
+    <div>
+        <div>Страна</div>
+        <div className='answers-row'>
+            {countryes.map(countrie => 
+                <div key={countrie} className={changeStyle('country', countrie)} onClick={()=>onChangeAnswer('country', countrie)}>{countrie}</div>
+            )}
+        </div>
+    </div>
+    <div>
+        <div>Автор</div>
+        <div className='answers-row'>
+            {authors.map(author => <div key={author} className={changeStyle('author', author)} onClick={()=>onChangeAnswer('author', author)}>
+                {author}
+            </div>)}
+        </div>
+    </div>
+    <div>
+        <div>Название</div>
+        <div>
+            {names.map(name => <div key={name} className={changeStyle('name', name)} onClick={()=>onChangeAnswer('name', name)}>{name}</div>)}
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default Answers;
